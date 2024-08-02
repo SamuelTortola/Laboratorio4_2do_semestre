15fe70c0fdbcf9f77d611f52c6c665f67ccca970
Alan Gomez <157183000+KleperForest@users.noreply.github.com>
2024-08-02 00:44:00 -0600

    j

--- Esclavo 2_botones/I2C/I2C.h
+++ Esclavo 2_botones/I2C/I2C.h
@@ -1,9 +1,18 @@
 
-
-
 #ifndef I2C_H_
 #define I2C_H_
 
+#include <avr/io.h>
+#include <stdbool.h>
+#define F_CPU 16000000
+
+void I2C_Config_MASTER(uint8_t Prescaler, unsigned long SCL_Clock);
+void I2C_Config_SLAVE(uint8_t address);
+void I2C_esclavo(uint8_t dato);
+uint8_t I2C_inicio();
+uint8_t I2C_EscByte(uint8_t dato);
+void I2C_STOP();
+uint8_t I2C_READ(uint8_t *dato, uint8_t ack);
 
 
 
